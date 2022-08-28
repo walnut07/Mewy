a96b27dfb2f0f1dd7a3d0a308c5106e9616e7a17
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-28 21:06:11 +0900

    Feat: Add useNavigate to enable screen transition

--- client/src/components/molecules/buttons/Explore.tsx
+++ client/src/components/molecules/buttons/Explore.tsx
@@ -1,10 +1,17 @@
 import "../../../Style.css";
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from "react-router-dom"
 
 const Explore = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/list", { state: { id: 1 }})
+  }
+
   return (
-    <Button variant="primary" type="submit">
-      Explore cats
+    <Button variant="primary" type="button" onClick={handleClick}>
+      Explore
     </Button>
   );
 };
--- client/src/components/molecules/buttons/Post.tsx
+++ client/src/components/molecules/buttons/Post.tsx
@@ -1,11 +1,18 @@
 import "../../../Style.css";
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from "react-router-dom"
 
 const Post = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/post", { state: { id: 1 }})
+  }
+
   return (
-    <Button variant="primary" type="submit">
-      Post my cat
+    <Button variant="primary" type="button" onClick={handleClick}>
+      Post
     </Button>
   );
 };
