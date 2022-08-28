a8b9897067fe8c453ba07946bef46c56927722f6
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-28 13:08:19 +0900

    Feat: Split Home screen with Bootstrap

---
+++ client/src/components/organisms/Cards.tsx
@@ -0,0 +1,7 @@
+const Cards = () => {
+  return (
+      <h1>Cards</h1>
+  );
+};
+
+export default Cards;
--- client/src/components/templates/Home.tsx
+++ client/src/components/templates/Home.tsx
@@ -1,7 +1,26 @@
+import "./Home.css";
+import Title from "../organisms/Title";
+import Cards from "../organisms/Cards";
 
 const Home = () => {
   return (
-      <h1>This is Home</h1>
+    <div className="d-md-flex h-md-100 align-items-center">
+
+      <div className="col-md-6 p-0 bg-indigo h-md-100">
+          <div className="text-white d-md-flex align-items-center h-100 p-5 text-center justify-content-center">
+              <div className="logoarea pt-5 pb-5">
+                <Title />
+              </div>
+          </div>
+      </div>
+
+      <div className="col-md-6 p-0 bg-white h-md-100 loginarea">
+          <div className="d-md-flex align-items-center h-md-100 p-5 justify-content-center">
+              <Cards />
+          </div>
+      </div>
+        
+    </div>
   );
 };
 
