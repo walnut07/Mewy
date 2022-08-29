267d2b2f3b862b87a678506180097761021a132a
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-29 21:29:14 +0900

    Feat: Add auth context for user

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,21 @@ import Home from "./components/templates/Home";
 import List from "./components/templates/List";
 import Post from "./components/templates/Post";
 import Single from "./components/templates/SinglePhoto";
+import { AuthProvider } from './context/Authcontext';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/list" element={<List />}/>
-          <Route path="/post" element={<Post />}/>
-          <Route path="/single" element={<Single />}/>
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/list" element={<List />}/>
+            <Route path="/post" element={<Post />}/>
+            <Route path="/single" element={<Single />}/>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
