1bedb1324ddd9f5604dcc8d05133f866e589844d
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-29 16:29:40 +0900

    Feat: Add instant sign in and sign up feature

--- internal/server/router.go
+++ internal/server/router.go
@@ -13,12 +13,9 @@ func setRouter() *gin.Engine {
 	// Create API route group
 	api := router.Group("/api")
 	{
-		// Add /hello GET route to router and define route handler function
-		api.GET("/hello", func(ctx *gin.Context) {
-			ctx.JSON(200, gin.H{"msg": "world"})
-		})
+		api.POST("/signup", signUp)
+		api.POST("/signin", signIn)
 	}
-
 	router.NoRoute(func(ctx *gin.Context) { ctx.JSON(http.StatusNotFound, gin.H{}) })
 
 	return router
