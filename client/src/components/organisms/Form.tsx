78d30289dcdd795a57c871fe75f5ccf9a70599d7
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-30 14:31:11 +0900

    Feat: Create a Form component that has the entire Photo post form

--- client/src/components/organisms/Form.tsx
+++ client/src/components/organisms/Form.tsx
@@ -2,13 +2,15 @@ import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../../context/Authcontext";
 import SignInButton from "../molecules/buttons/SignIn";
 import SignUpButton from "../molecules/buttons/SignUp";
+import PostForm from "../molecules/PostForm";
+import UserName from "../molecules/UserName";
 
 const Form = () => {
   const { user } = useAuthContext();
   return (
     <section>
-      <h1>Form is being built</h1>
-      {/* TODO: Add a form to post a photo */}
+      {user && <UserName />}
+      <PostForm />
       {!user && 
         <>
         <SignInButton />
